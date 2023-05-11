4b1ce0e6bad3a3fca2a5fc537841bacfb92a6e38
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-05-11 18:15:06 +0200

    Removed redux-persist

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import { Provider } from 'react-redux';
-import { store, persistor } from '../redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from '../redux/store';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import '../styles/globals.css';
@@ -10,12 +9,10 @@ const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </PersistGate>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Provider>
   );
 }
